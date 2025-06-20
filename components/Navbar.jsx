5c3862e4880286a8f72a6f19a50d0fae769b9b3e
AdityaJogdand <160385303+AdityaJogdand@users.noreply.github.com>
2025-06-20 11:27:22 +0530

    test

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ import {useState} from 'react'
 function Navbar() {
   return (
     <nav class='w-full bg-black flex justify-between items-center'>
-      <img src="" alt="" />
+      
     </nav>
   )
 }
