3c97db1485654d19d3aad93baf9e368b2adf4c03
AdityaJogdand <160385303+AdityaJogdand@users.noreply.github.com>
2025-06-20 11:16:10 +0530

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+import {useState} from 'react'
+
+function Navbar() {
+  return (
+    <nav class='w-full bg-black flex justify-between items-center'>
+      <img src="" alt="" />
+    </nav>
+  )
+}
+
+export default Navbar
